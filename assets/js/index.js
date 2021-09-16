1c31b72654d5a9900b3b3723a78c1431b24a25ce
SvetaZaitseva <svitlana.zaitseva@autto.com>
2021-09-16 21:06:06 +0300

    add en, add animation

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// this.I18n = function (defaultLang) {
+//     var lang = defaultLang || 'en';
+//     this.language = lang;
+// }
+// this.I18n();
+// console.log()
+//
+function init() {
+    var menu = document.querySelectorAll('.menu');
+    // console.log(menu)
+}
+
+init();
