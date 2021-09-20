a800925ccb4dae7bd7d8739303b15deb4899de65
SvetaZaitseva <svitlana.zaitseva@autto.com>
2021-09-20 21:18:44 +0300

    add adaptive, add new text

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,30 @@
 // this.I18n();
 // console.log()
 //
+var state = 'close';
+var sidebar = document.querySelector('.sidebar');
+var link = document.querySelectorAll('.menu-link');
 function init() {
-    var menu = document.querySelectorAll('.menu');
-    // console.log(menu)
+    link.forEach(function(el){
+        el.addEventListener('click', function (e) {
+            closeSidebar();
+        })
+    });
+    window.addEventListener('scroll', function(e) {
+        closeSidebar();
+    })
+}
+function changeStyle() {
+    if(state === 'close') {
+        state = 'open';
+        sidebar.style.display = 'block'
+    } else {
+        closeSidebar();
+    }
 }
 
+function closeSidebar() {
+    state = 'close';
+    sidebar.style.display = 'none'
+}
 init();
